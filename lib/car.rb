595cebd1a510f7aa89577e378ecca08db72999b1
emilykim13 <emilyn0614@gmail.com>
2021-03-03 13:04:08 -0600

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,14 @@
+require 'pry'
 require_relative "./vehicle.rb"
+class Car < Vehicle #subclass
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+    def get_out
+        "vrm vrm get out me mum's car"
+    end
+    # so basically 'Car' is a subclass of 'Vehicle' and its instance methods are not shared with the parent class 'Vehicle' 
+    # BUT the subclass can use the instance methods from the parent class
+end 
+# binding.pry
